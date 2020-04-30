e98872569b47f3a1393dcfec57a9f9f527e94410
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-04-30 16:37:10 +0500

    The application is paired with urlScheme app

--- urlScheme2/ViewController.swift
+++ urlScheme2/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
