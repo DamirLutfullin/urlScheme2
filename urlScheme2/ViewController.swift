01ed692672bb0ed6f226ffa3077966a799c463f0
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-04-30 11:32:53 +0500

    Initial Commit

---
+++ urlScheme2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  urlScheme2
+//
+//  Created by Damir Lutfullin on 30.04.2020.
+//  Copyright © 2020 DamirLutfullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
